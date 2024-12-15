7cd23079bda3e4d457ad9e8545d85a53ec2b6c83
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-12-15 09:08:12 +0300

    add jinja2 & html & css

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Подключение папки со статическими файлами
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+# Настройка шаблонов
+templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "message": "Hello, Jinja2!"}
+    )
