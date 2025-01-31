5d4559d52510fe633a36dab4eb3fa2149ab362e7
ArtemOvsyannikov <artemrotfront@gmail.com>
2025-01-31 14:47:00 +0300

    add test content for main page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuItems.forEach(item => {
         item.addEventListener('click', function () {
-            menuItems.forEach(i => i.classList.remove('active')); // Убираем active у всех
-            this.classList.add('active'); // Добавляем active только на кликнутый элемент
+            menuItems.forEach(i => i.classList.remove('active'));
+            this.classList.add('active'); 
         });
     });
 });
