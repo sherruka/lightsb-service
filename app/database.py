f999ed87005324802fc98d86eac3855c0b30bc2e
Ruslan Sheykh <ruslanqas@yandex.ru>
2025-02-27 13:26:55 +0300

    add database connection setup

---
+++ app/database.py
@@ -0,0 +1,24 @@
+from typing import Any, Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm.session import Session
+import os
+
+POSTGRES_USER = os.getenv("POSTGRES_USER", "default_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "default_password")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "default_port")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "default_db")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db() -> Generator[Session, Any, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
