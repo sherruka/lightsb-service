1381c5146d53a9972acf8fd6d24da8fc97e755f3
Sheykh Ruslan <ruslanqas@yandex.ru>
2025-03-13 15:35:14 +0300

    generate access & refresh tokens on login, implement token refresh

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 from app.resources.user import router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://localhost"],  # Разрешённые источники
+    allow_credentials=True,  # Важно для работы с куками
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="/app/templates")
 
--- app/static/js/login.js
+++ app/static/js/login.js
@@ -13,23 +13,60 @@ document.addEventListener("DOMContentLoaded", async function () {
                 formObject[key] = value;
             });
 
-            // Отправка данных на сервер
-            let response = await fetch("/api/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(formObject) // Отправляем данные как JSON
-            });
+            try {
+                // Отправка данных на сервер
+                let response = await fetch("/api/auth/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(formObject), // Отправляем данные как JSON
+                    credentials: "include",
+                });
 
-            let result = await response.json();
+                let result = await response.json();
 
-            if (response.ok && result.redirect_to) {
-                // Перенаправление на нужную страницу после регистрации
-                window.location.href = `/?redirect=${result.redirect_to}`;
-            } else {
-                alert(result.detail || "Login failed");
+                if (response.ok && result.redirect_to && result.access_token) {
+                    // Сохраняем токен в localStorage
+                    sessionStorage.setItem("access_token", result.access_token);
+
+                    // Перенаправление на нужную страницу после регистрации
+                    window.location.href = `/?redirect=${result.redirect_to}`;
+                } else {
+                    alert(result.detail || "Login failed");
+                }
+            }catch (error) {
+                    console.error("Error during login:", error);
+                    alert("Something went wrong. Please try again later.");
             }
         });
     }
 });
+
+async function refreshAccessToken() {
+
+    try{
+        const response = await fetch("/api/auth/refresh", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            sessionStorage.setItem("access_token", result.access_token);
+            return result.access_token;
+        } else {
+            // Если refresh token невалиден, пользователь должен заново войти
+            alert("Session expired, please log in again.");
+            sessionStorage.removeItem("access_token");
+            window.location.href = "/api/auth/login";
+        }
+    } catch (error) {
+        console.error("Error refreshing token:", error);
+        alert("Something went wrong. Please log in again.");
+        window.location.href = "/api/auth/login";
+    }
+}
