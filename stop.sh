05057677cf0148e0f7771357ea17cb6c094df98f
Sheykh Ruslan <ruslanqas@yandex.ru>
2025-05-23 16:17:31 +0300

    Integrate load testing into project and set up GitHub Actions CI

--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@ docker compose \
     -f ./docker/docker-compose.FastAPI.yaml \
     -f ./docker/docker-compose.nginx.yaml \
     -f ./docker/docker-compose.postgres.yaml \
+    -f ./docker/docker-compose.locust.yaml \
     --env-file ./env/.env.minio \
     --env-file ./env/.env.backend down
 
