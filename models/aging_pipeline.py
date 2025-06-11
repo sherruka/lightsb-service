b2434c86469e118ed3512c1806a3a1684653bd18
andrey <aeksin06.05.2004@gmail.com>
2025-06-11 15:59:04 +0300

    greatly reduce false positives rate and remove duplicates of head detection pipeline

--- models/aging_pipeline.py
+++ models/aging_pipeline.py
@@ -1,9 +1,8 @@
 import os.path
 
+from generator import generate
+from head_detection import head_detector
 from ultralytics import YOLO
-
-from models.generator import generate
-from models.head_detection import head_detector
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -21,14 +20,23 @@ def aging_pipeline(
     model = YOLO(checkpoint_path)
     hd_model = head_detector(model)
     detected_heads = hd_model.predict(filenames)
-    if len(detected_heads) == 0:
-        print("no heads was detected on given image")
-        return ["no heads was detected on given image"]
+
     aged_people = generate(detected_heads, output_path, result_count, delta)
+
+    detected__heads_files = os.scandir(f"{SCRIPT_PATH}/head_detection_results/")
+    for file in detected__heads_files:
+
+        if file.is_file() and os.path.exists(file):
+            os.remove(file)
+
     return aged_people
 
 
 if __name__ == "__main__":
     # for testing purposes
-    ret = aging_pipeline("./dataset_samples/faces/realign1024x1024/00152.png")
-    print(ret)
+    files = os.scandir("./models/dataset_samples/faces/realign1024x1024")
+    for entry in files:
+        if entry.is_file() and entry.name.endswith("png"):
+            ret = aging_pipeline(entry.path)
+            # print(ret)
+    # aging_pipeline([file.path for file in files])
